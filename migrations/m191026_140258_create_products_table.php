4d8cd72d4781188cc924a371ed8a8b914f630b7a
TimKast <tamerlan8.05.92@gmail.com>
2019-10-26 17:04:31 +0300

    add migrations for categories and products tables

---
+++ migrations/m191026_140258_create_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%products}}`.
+ */
+class m191026_140258_create_products_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%products}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(512)->notNull(),
+            'description' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%products}}');
+    }
+}
